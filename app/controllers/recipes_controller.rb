c013344aab16fc18429b29095437edc52e4a5b72
Cristian Zambrano <criszamcham@gmail.com>
2023-09-06 17:33:41 -0400

    Add recipe method created

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,4 +20,20 @@ class RecipesController < ApplicationController
       render 'show'
     end
   end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+
+    if @recipe.save
+      redirect_to user_recipe_path(current_user, @recipe), notice: "Recipe created successfully."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public)
+  end
 end
