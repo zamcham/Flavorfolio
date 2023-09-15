65760ac1faf19bc23a5f709283afb61ec6fff10e
Cristian Zambrano <criszamcham@gmail.com>
2023-09-15 10:11:28 -0700

    Update precompile config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,9 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.assets.debug = true
+  config.assets.compile = true
+  
   # Do not eager load code on boot.
   config.eager_load = false
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [] do
     resources :recipes, only: [:new, :create, :index, :show, :destroy]
+    resources :ingredients, only: [:index, :show, :edit, :update, :destroy]
     member do
       post 'toggle_visibility'
     end
