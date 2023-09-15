a48cd3f53d8ab61c36de766953d622a4166c18f9
Cristian Zambrano <criszamcham@gmail.com>
2023-09-15 12:56:14 -0700

    Create new ingredient base

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [] do
     resources :recipes, only: [:new, :create, :index, :show, :destroy]
-    resources :ingredients, controller: 'ingredients_ownership', only: [:index, :show, :edit, :update, :destroy]
+    resources :ingredients, controller: 'ingredients_ownership', only: [:index, :new, :edit, :update, :destroy]
     member do
       post 'toggle_visibility'
     end
