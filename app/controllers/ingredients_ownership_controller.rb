22c0c55b2861b66e2e0b3c1e3dcb87473b78eab9
Cristian Zambrano <criszamcham@gmail.com>
2023-09-15 14:45:31 -0700

    Add ingredient test 1

--- app/controllers/ingredients_ownership_controller.rb
+++ app/controllers/ingredients_ownership_controller.rb
@@ -3,4 +3,9 @@ class IngredientsOwnershipController < ApplicationController
         user_id = params[:user_id]
         @ingredients = IngredientOwnership.where(user_id: user_id)
     end
+
+    def new
+        @ingredient_ownership = current_user.ingredient_ownerships.build
+        @ingredients = Ingredient.all
+    end
 end
