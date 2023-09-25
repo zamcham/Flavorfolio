70e6cad64735af5ece2c76be94fbb93ca8149f2c
Cristian Zambrano <criszamcham@gmail.com>
2023-09-25 15:58:38 -0700

    Create recipe ingredient association

--- db/migrate/20230820032241_create_ingredient_ownership.rb
+++ db/migrate/20230820032241_create_ingredient_ownership.rb
@@ -3,9 +3,7 @@ class CreateIngredientOwnership < ActiveRecord::Migration[7.0]
     create_table :ingredient_ownerships do |t|
       t.references :user, foreign_key: true
       t.references :ingredient, foreign_key: true
-      t.references :recipe, foreign_key: true
-      t.decimal :user_quantity, precision: 10, scale: 2
-      t.decimal :recipe_quantity, precision: 10, scale: 2
+      t.decimal :quantity, precision: 10, scale: 2
 
       t.timestamps
     end
---
+++ db/migrate/20230925225422_create_recipes_ingredients_join_table.rb
@@ -0,0 +1,8 @@
+class CreateRecipesIngredientsJoinTable < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :recipes, :ingredients do |t|
+      t.index [:recipe_id, :ingredient_id]
+      t.index [:ingredient_id, :recipe_id]
+    end
+  end
+end
